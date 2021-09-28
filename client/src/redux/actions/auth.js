23c99374c79a953f72dbf9eb39def58098fd8834
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-09-28 10:52:22 +0530

    Login logic for all kind of users,

--- client/src/redux/actions/auth.js
+++ client/src/redux/actions/auth.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { DEVELOPER_CATEGORY, SOCIETY_CATEGORY } from "../../constants/strings";
 import apolloClient from "../../utils/apollo-client";
 import {
   API_CALL_TRIGGERED,
@@ -14,26 +15,57 @@ export const login =
     dispatch({
       type: API_CALL_TRIGGERED,
     });
+    let query;
+    if (userCategory === DEVELOPER_CATEGORY) {
+      query = gql`
+        query loginDeveloper($email: String!, $password: String!) {
+          loginDeveloper(email: $email, password: $password) {
+            _id
+            token
+            expiresIn
+          }
+        }
+      `;
+    } else if (userCategory === SOCIETY_CATEGORY) {
+      query = gql`
+        query loginSociety($email: String!, $password: String!) {
+          loginSociety(email: $email, password: $password) {
+            _id
+            token
+            expiresIn
+          }
+        }
+      `;
+    } else {
+      query = gql`
+        query loginMember($email: String!, $password: String!) {
+          loginMember(email: $email, password: $password) {
+            _id
+            token
+            expiresIn
+          }
+        }
+      `;
+    }
+
     try {
       const res = await apolloClient.query({
-        query: gql`
-          query loginDeveloper($email: String!, $password: String!) {
-            loginDeveloper(email: $email, password: $password) {
-              _id
-              token
-              expiresIn
-            }
-          }
-        `,
+        query: query,
         variables: { email, password },
       });
+      let data;
+      if (userCategory === DEVELOPER_CATEGORY) {
+        data = res.data?.loginDeveloper;
+      } else if (userCategory === SOCIETY_CATEGORY) {
+        data = res.data?.loginSociety;
+      } else {
+        data = res.data?.loginMember;
+      }
       // console.log(res.data?.loginDeveloper);
-      await AsyncStorage.setItem("token", res.data?.loginDeveloper?.token);
+      await AsyncStorage.setItem("token", data.token);
       await AsyncStorage.setItem(
         "expiresIn",
-        new Date(
-          new Date().getTime() + res.data?.loginDeveloper?.expiresIn * 1000
-        ).toISOString()
+        new Date(new Date().getTime() + data.expiresIn * 1000).toISOString()
       );
       await AsyncStorage.setItem("userCategory", userCategory);
       dispatch(loadUserMetaData());
@@ -45,7 +77,7 @@ export const login =
 
 export const loadUserMetaData = () => async (dispatch) => {
   try {
-    // await AsyncStorage.clear();
+    await AsyncStorage.clear();
     const token = await AsyncStorage.getItem("token");
     const expiresIn = new Date(await AsyncStorage.getItem("expiresIn"));
     const userCategory = await AsyncStorage.getItem("userCategory");
